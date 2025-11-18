f1678e7abf0f0a0b2e0fd2119b7bd5fe552e2b79
ikramiiss <ikramhossian22@gmail.com>
2025-11-18 14:29:24 +0600

    add five and six problem

--- solution.ts
+++ solution.ts
@@ -102,3 +102,25 @@ console.log(filterActiveUsers(users));
 
 /* --------------------------------------------------------------------- */
 
+interface Book {
+  title: string;
+  author: string;
+  publishedYear: number;
+  isAvailable: boolean;
+}
+
+function printBookDetails(book: Book): void {
+  const availability = book.isAvailable ? "Yes" : "No";
+  console.log(
+    `Title: ${book.title}, Author: ${book.author}, Published: ${book.publishedYear}, Available: ${availability}`
+  );
+}
+
+const myBook: Book = {
+  title: "The Great Gatsby",
+  author: "F. Scott Fitzgerald",
+  publishedYear: 1925,
+  isAvailable: true,
+};
+
+printBookDetails(myBook);
